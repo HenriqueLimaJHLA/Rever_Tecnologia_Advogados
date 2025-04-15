a3cc22b5514fbdf86290b03f897c04c5c1a17ac3
José Henrique <josehenriquefnbr@gmail.com>
2025-04-15 16:25:28 -0300

    dashboard.create---pages/index.php

---
+++ pages/index.php
@@ -0,0 +1,44 @@
+<?php 
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard | Rever Tecnologia | Advogados</title>
+    <link rel="stylesheet" href="../frontend/src/css/dashboard.css">
+    <link rel="shortcut icon" href="../frontend/assets/rever.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+</head>
+</head>
+<body>
+    <div class="container-dashboard">
+        <nav class="dashboard">
+            <div class="logo-dashboard">
+                <img src="../frontend/assets/rever.png" alt="Logo Rever Tecnologia" class="logo-rever">
+                <h6>Rever Tecnologia</h6>
+            </div>
+            <ul class="menu-dashboard">
+                <li><span class="material-symbols-outlined">home</span></li>
+                <li><span class="material-symbols-outlined">gavel</span></li>
+                <li><span class="material-symbols-outlined">home</span></li>
+                <li><span class="material-symbols-outlined">home</span></li>
+            </ul>
+            
+            <div class="container-functions">
+                <span class="material-symbols-outlined setttings">settings</span>
+                <span class="material-symbols-outlined logout">logout</span>
+            </div>
+                
+        </nav>
+    </div>
+</body>
+<!-- Bibliotecas de Importação -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</html>
