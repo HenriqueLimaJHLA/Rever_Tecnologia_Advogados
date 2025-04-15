1cace6e1a3e6cc9997c9e0db0b9c2524e50c5432
José Henrique <josehenriquefnbr@gmail.com>
2025-04-15 15:19:24 -0300

    design.css--finally

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -3,5 +3,10 @@ const formLogin = document.getElementById('form_login');
 const emailLogin = document.getElementById('email_login');
 const passwordLogin = document.getElementById('password_login');
 const aForgetPassword = document.getElementById('forget_password');
+const h2Login = document.getElementById('h2_login');
 
 // Eventos 
+aForgetPassword.addEventListener('click', () => {
+    alert('');
+});
+
--- index.php
+++ index.php
@@ -9,17 +9,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | Rever Tecnologia | Advogados</title>
     <link rel="stylesheet" href="./frontend/src/css/index.css">
-    <link rel="shortcut icon" href="./frontend/assets/rever-logo.webp" type="image/x-icon">
+    <link rel="shortcut icon" href="./frontend/assets/rever.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 
 <body>
     <div class="container-form">
-        <h2>Login</h2>
-        <p>Bem-vindo de volta! Insira suas credenciais para acessar sua conta.</p>
+        <h2 id="h2_login">Login</h2>
+        <p id="p_login">Bem-vindo de volta! Insira suas credenciais para acessar sua conta.</p>
         <form class="login-form" id="form_login">
-            <img src="./frontend/assets/rever-logo.webp" class="logo-rever-tecnologia" alt="Rever Tecnologia">
+            <img src="./frontend/assets/rever-logo.webp" id="img_rever" class="logo-rever-tecnologia" alt="Rever Tecnologia">
             <input type="email" class="form-control" id="email_login" placeholder="Insira o seu email" required autocomplete="off">
             <input type="password" class="form-control" id="password_login" placeholder="Insira a sua senha" required autocomplete="off">
             <input type="hidden" name="csrf_token" value="<?= uniqid() ?>">
@@ -35,7 +35,7 @@
     </div>
 
 </body>
-<script src="./frontend/src/js/index.js"></script>
+<script src="./frontend/src/js/script.js"></script>
 <!-- Bibliotecas de Importação -->
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
