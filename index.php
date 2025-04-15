0259c33c52c9b392c6f2cb66a697036e332c9635
José Henrique <josehenriquefnbr@gmail.com>
2025-04-15 13:45:07 -0300

    ---restart.tailwindcss error

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php 
-
 ?>
 
 <!DOCTYPE html>
@@ -8,8 +7,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./frontend/src/css/tailwind_config_output.css">
 </head>
 <body>
-    
+    <h1 class="text-3xl font-bold underline text-center text-red-500">
+    Hello world!
+    </h1>
 </body>
 </html>
